d860f7cca6ded7e892fc8ce280b89b773eeda9c6
María Arias de Reyna Domínguez <mariasde@ibm.com>
2025-10-10 11:43:59 +0200

    feat: Add verbose to describe command

--- src/main/java/tooling/leyden/commands/DescribeCommand.java
+++ src/main/java/tooling/leyden/commands/DescribeCommand.java
@@ -4,7 +4,9 @@ import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import tooling.leyden.aotcache.Element;
 import tooling.leyden.aotcache.ReferencingElement;
+import tooling.leyden.commands.autocomplete.Packages;
 
+import java.sql.Ref;
 import java.util.List;
 
 @Command(name = "describe", mixinStandardHelpOptions = true,
@@ -15,6 +17,13 @@ class DescribeCommand implements Runnable {
 
 	@CommandLine.ParentCommand
 	DefaultCommand parent;
+
+	@CommandLine.Option(names = {"-v", "--verbose"},
+			description = {"Show the extended information, like the full list of related elements."},
+			arity = "0..*",
+			defaultValue = "false",
+			paramLabel = "<verbose>")
+	protected Boolean verbose;
 
 	@CommandLine.Mixin
 	private CommonParameters parameters;
@@ -28,11 +37,41 @@ class DescribeCommand implements Runnable {
 				var leftPadding = "|  ";
 				parent.getOut().println("-----");
 				parent.getOut().println(e.getDescription(leftPadding));
+				if(verbose) {
+					if (e instanceof ReferencingElement re) {
+						if (!re.getReferences().isEmpty()) {
+							parent.getOut().println(leftPadding + "Elements referenced from this element: ");
+							parent.getOut().println(leftPadding + "  _____");
+							re.getReferences().forEach(refer -> parent.getOut().println(leftPadding + "  | " + refer.toString()));
+							parent.getOut().println(leftPadding + "  _____");
+						} else {
+							parent.getOut().println(leftPadding + "There are no elements referenced from this element.");
+						}
+					}
+
+					var referring = getElementsReferencingThisOne(e);
+					if (!referring.isEmpty()) {
+						parent.getOut().println(leftPadding + "Elements that refer to this element: ");
+						parent.getOut().println(leftPadding + "  _____");
+						referring.forEach(refer -> parent.getOut().println(leftPadding + "  | " + refer.toString()));
+						parent.getOut().println(leftPadding + "  _____");
+					} else {
+						parent.getOut().println(leftPadding + "There are no other elements of the cache that refer " +
+								"to this element.");
+					}
+				}
 				parent.getOut().println("-----");
 			});
 		} else {
 			parent.getOut().println("ERROR: Element not found. Try looking for it with ls.");
 		}
+	}
+
+	public List<Element> getElementsReferencingThisOne(Element element) {
+		return parent.getInformation().getAll().parallelStream()
+				.filter(e -> (e instanceof ReferencingElement))
+				.filter(e -> ((ReferencingElement) e).getReferences().contains(element))
+				.toList();
 	}
 
 
