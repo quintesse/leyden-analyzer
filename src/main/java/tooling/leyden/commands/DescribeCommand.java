da6c1ca013bf0f475a34464893296cc6a1c0aa13
María Arias de Reyna Domínguez <mariasde@ibm.com>
2025-10-10 11:34:59 +0200

    feat: Simplify describe command

--- src/main/java/tooling/leyden/commands/DescribeCommand.java
+++ src/main/java/tooling/leyden/commands/DescribeCommand.java
@@ -28,26 +28,11 @@ class DescribeCommand implements Runnable {
 				var leftPadding = "|  ";
 				parent.getOut().println("-----");
 				parent.getOut().println(e.getDescription(leftPadding));
-				var referring = getElementsReferencingThisOne(e);
-				if (!referring.isEmpty()) {
-					parent.getOut().println(leftPadding + "There are other elements of the cache that link " +
-							"to this element: ");
-					parent.getOut().println(leftPadding + "  _____");
-					referring.forEach(refer -> parent.getOut().println(leftPadding + "  | " + refer.toString()));
-					parent.getOut().println(leftPadding + "  _____");
-				}
 				parent.getOut().println("-----");
 			});
 		} else {
 			parent.getOut().println("ERROR: Element not found. Try looking for it with ls.");
 		}
-	}
-
-	public List<Element> getElementsReferencingThisOne(Element element) {
-		return parent.getInformation().getAll().parallelStream()
-				.filter(e -> (e instanceof ReferencingElement))
-				.filter(e -> ((ReferencingElement) e).getReferences().contains(element))
-				.toList();
 	}
 
 
