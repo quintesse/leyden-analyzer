f3cacb858d2462545756f8e7be229eb479b36f79
María Arias de Reyna Domínguez <mariasde@ibm.com>
2025-10-21 12:05:35 +0200

    feat: auto-detect packages with methods not trained but maybe should have been trained

--- src/main/java/tooling/leyden/commands/WarningCommand.java
+++ src/main/java/tooling/leyden/commands/WarningCommand.java
@@ -6,6 +6,7 @@ import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import tooling.leyden.aotcache.ClassObject;
 import tooling.leyden.aotcache.Element;
+import tooling.leyden.aotcache.MethodObject;
 import tooling.leyden.aotcache.Warning;
 import tooling.leyden.aotcache.WarningType;
 
@@ -74,7 +75,10 @@ class WarningCommand implements Runnable {
 	@Command(mixinStandardHelpOptions = true,
 			version = "1.0",
 			subcommands = {CommandLine.HelpCommand.class},
-			description = "Automatically try to detect inconsistencies and potential defects in your run.")
+			description = {"Automatically try to detect inconsistencies and potential defects in your run.",
+					"The auto-detected issues may or may not be problematic.",
+					"It is up to the developer to decide that."
+			})
 	public void check() {
 
 		parent.getOut().println("Trying to detect problems...");
@@ -83,43 +87,53 @@ class WarningCommand implements Runnable {
 		warnings.clear();
 
 		warnings.addAll(getTopTenPackagesNotCached());
+		warnings.addAll(getTopTenPackagesUsedAndNotTrained());
 
 		printWarnings();
 		parent.getOut().println("The auto-detected issues may or may not be problematic.");
 		parent.getOut().println("It is up to the developer to decide that.");
 	}
 
+	// Get top ten packages with most methods called but not trained
+	private List<Warning> getTopTenPackagesUsedAndNotTrained() {
+		var result = new ArrayList<Warning>();
+		var packages = new HashMap<String, Integer>();
+
+		parent.getInformation().getElements(null, null, excludedPackages, false, false, "Method")
+				.map(MethodObject.class::cast)
+				.filter(e -> e.getMethodCounters() != null)
+				.filter(e -> e.getMethodTrainingData() == null || e.getCompileTrainingData().isEmpty())
+				.forEach(method -> {
+					final var classObject = method.getClassObject();
+					addToPackageList(classObject, packages);
+				});
+
+		String warningString = " methods that were called during training run but lack proper training (don't have " +
+				"TrainingData objects associated to them).";
+		getTopTenPackages(packages, warningString, result);
+
+		return result;
+	}
+
+	// Get top ten packages with most classes not cached
 	private List<Warning> getTopTenPackagesNotCached() {
 		var result = new ArrayList<Warning>();
 		var packages = new HashMap<String, Integer>();
 
 		for (Element e : parent.getInformation().getExternalElements().values()) {
 			if (e instanceof ClassObject classObject) {
-				//Cut package name to just three levels (dots)
-				//because we are looking for base packages
 				if (Arrays.stream(excludedPackages).noneMatch(p -> classObject.getPackageName().startsWith(p))) {
-					String packageName = "";
-					int i = 1;
-					for (String s : classObject.getPackageName().split("\\.")) {
-						if (packageName.isBlank()) {
-							packageName += s;
-						} else {
-							packageName += "." + s;
-						}
-						if (i++ > 2) {
-							break;
-						}
-					}
-					packages.putIfAbsent(packageName, 0);
-					packages.compute(packageName, (key, v) -> {
-						v++;
-						return v;
-					});
+					addToPackageList(classObject, packages);
 				}
 			}
 		}
 
-		// Get top ten packages with most classes not cached
+		getTopTenPackages(packages, " classes loaded and not cached.", result);
+
+		return result;
+	}
+
+	private static void getTopTenPackages(HashMap<String, Integer> packages, String warningString, ArrayList<Warning> result) {
 		packages.entrySet().stream().sorted((e1, e2) -> e2.getValue() - e1.getValue()).limit(10)
 				.forEach(entry -> {
 					AttributedStringBuilder sb = new AttributedStringBuilder();
@@ -128,14 +142,34 @@ class WarningCommand implements Runnable {
 					sb.append(entry.getKey());
 					sb.style(AttributedStyle.DEFAULT);
 					sb.append("' contains ");
-					sb.style(AttributedStyle.DEFAULT.bold().foreground(AttributedStyle.RED).background(AttributedStyle.WHITE));
+					sb.style(AttributedStyle.DEFAULT.bold().foreground(AttributedStyle.RED));
 					sb.append(entry.getValue().toString());
 					sb.style(AttributedStyle.DEFAULT);
-					sb.append(" classes not cached.");
+					sb.append(warningString);
 					result.add(new Warning(null, sb.toAttributedString(), WarningType.Training));
 				});
+	}
 
-		return result;
+	private static void addToPackageList(ClassObject classObject, HashMap<String, Integer> packages) {
+		//Cut package name to just three levels (dots)
+		//because we are looking for base packages
+		String packageName = "";
+		int i = 1;
+		for (String s : classObject.getPackageName().split("\\.")) {
+			if (packageName.isBlank()) {
+				packageName += s;
+			} else {
+				packageName += "." + s;
+			}
+			if (i++ > 2) {
+				break;
+			}
+		}
+		packages.putIfAbsent(packageName, 0);
+		packages.compute(packageName, (key, v) -> {
+			v++;
+			return v;
+		});
 	}
 
 	static class Types implements Iterable<String> {
